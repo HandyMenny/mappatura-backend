fbea0746dbd4163be99c6740066d6ce875fb6ec2
Edoardo Ranghieri <edoardo@ranghieri.com>
2021-08-07 20:11:20 +0200

    Fix PM2 CI Action

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -17,7 +17,7 @@ module.exports = {
       repo: "git@github.com:theedoran/mappatura2021-backend.git",
       path: "/home/theedoran/mappatura2021-backend",
       "post-deploy":
-        "npm i && pm2 reload ecosystem.config.js --env production && pm2 save && git checkout package-lock.lock",
+        "npm i && pm2 reload ecosystem.config.js --env production && pm2 save && git checkout package-lock.json",
       env: {
         NODE_ENV: "production",
       },
