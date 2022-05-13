af433acc314202c5f1c21cc6bb56710c97e727a1
handymenny <handymenny@outlook.com>
2022-05-13 20:38:07 +0200

    use new tar.xz files

--- init-csv.sh
+++ init-csv.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-ZIPFILE=("Consultazione2021.zip" "Consultazione2021_bianche2016.zip" "Bando1Giga-v1.zip" "Bando1Giga_TN_BZ-v2.zip")
+ZIPFILE=("Consultazione2021.tar.xz" "Consultazione2021_bianche2016.tar.xz" "Bando1Giga-v1.tar.xz" "Bando1Giga_TN_BZ-v2.tar.xz")
 DOWNLOAD_URL="https://media.githubusercontent.com/media/HandyMenny/database-infratel/main/"
 DIR=("csv/Consultazione2021" "csv/Consultazione2021Bianche"  "csv/Bando1Giga"  "csv/Bando1Giga")
 
@@ -9,22 +9,15 @@ if ! command -v wget &>/dev/null; then
   exit 1
 fi
 
-if ! command -v unzip &>/dev/null; then
-  echo "unzip is not found on the system, you must install it to execute this script"
+if ! command -v tar &>/dev/null; then
+  echo "tar is not found on the system, you must install it to execute this script"
   exit 1
 fi
 
 function download_extract() {
   wget "$2$1" -P $3/
-  while [ `ls -1 $3/*.zip 2>/dev/null | wc -l` != 0 ]
-  do
-    for file in $3/*.zip
-    do
-      unzip -jo "$file" -d $3/
-      rm "$file"
-    done
-  done
-  find $3/ ! -iname "*.csv" -type f -delete
+  tar xf $3/$1 --transform='s/.*\///' --directory $3/
+  find $3/ -mindepth 1 ! -iname "*.csv" -delete
   rm -f $3/header.csv
 }
 
